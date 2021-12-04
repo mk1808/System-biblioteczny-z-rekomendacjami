391844b11078627765b9218199892a768bab2f85
Monika Kordoń <monikor97@gmail.com>
2021-12-04 14:49:05 +0100

    #2 services, modules

--- library/src/main/ts/libraryBrs/src/app/shared/shared.module.ts
+++ library/src/main/ts/libraryBrs/src/app/shared/shared.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    LayoutComponent
+
   ]
 })
 export class SharedModule { }
