425db8a92b98a49674b94f94fbc516d1252055e7
Monika Kordoń <monikor97@gmail.com>
2021-12-23 22:55:24 +0100

    #5 adjusting elements

--- library/src/main/ts/libraryBrs/src/app/shared/elements/input/input.component.ts
+++ library/src/main/ts/libraryBrs/src/app/shared/elements/input/input.component.ts
@@ -7,6 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class InputComponent implements OnInit {
   @Input() label:any;
+  @Input() inputClass:any;
+  
   constructor() { }
 
   ngOnInit(): void {
--- library/src/main/ts/libraryBrs/src/app/shared/layout/main-content/two-columns-content/two-columns-content.component.ts
+++ library/src/main/ts/libraryBrs/src/app/shared/layout/main-content/two-columns-content/two-columns-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-two-columns-content',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./two-columns-content.component.scss']
 })
 export class TwoColumnsContentComponent implements OnInit {
-
+  @Input() leftClass="";
+  @Input() rightClass="";
   constructor() { }
 
   ngOnInit(): void {
