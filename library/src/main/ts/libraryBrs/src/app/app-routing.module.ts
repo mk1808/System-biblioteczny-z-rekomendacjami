56f296dc918c381d96454079b30582cfc426f6a8
Monika Kordoń <monikor97@gmail.com>
2021-12-24 04:39:50 +0100

    #7 my account view and nav

--- library/src/main/ts/libraryBrs/src/app/app-routing.module.ts
+++ library/src/main/ts/libraryBrs/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: 'home', loadChildren: () => import('./main-pages/main-pages.module').then(m => m.MainPagesModule) },
   { path: 'books', loadChildren: () => import('./book/book.module').then(m => m.BookModule) },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
   { path: '**', redirectTo: '/home',}
 ];
 
--- library/src/main/ts/libraryBrs/src/app/user/user.module.ts
+++ library/src/main/ts/libraryBrs/src/app/user/user.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MyAccountComponent } from './my-account/my-account.component';
+import { UserRoutingModule } from './user-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MyAccountComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    UserRoutingModule
   ]
 })
 export class UserModule { }
