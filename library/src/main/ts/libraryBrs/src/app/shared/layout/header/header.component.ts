f6e81c5a668b182632be8e2495bdaee783c9ae0b
Monika Kordoń <monikor97@gmail.com>
2021-12-26 04:39:01 +0100

    #2 changes in header

--- library/src/main/ts/libraryBrs/src/app/shared/layout/header/header.component.ts
+++ library/src/main/ts/libraryBrs/src/app/shared/layout/header/header.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+declare let $ : any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  ngAfterViewInit() {
+   
+    //let elements = this.element.nativeElement.querySelectorAll('.ui.dropdown');
+//    setTimeout(this.loadJQuery);
+    this.loadJQuery();
+ }
 
+ 
+ loadJQuery(){
+   /*debugger
+   let select:any = $('.search.dropdown')[0];
+    select.dropdown();
+    debugger*/
+    $('.menu.dmenu').dropdown();
+ }
 }
