77155f0fdc20a340ab203d1c9acb7b9062b73557
Monika Kordoń <monikor97@gmail.com>
2021-12-26 10:33:26 +0100

    #5 button without icon

--- library/src/main/ts/libraryBrs/src/app/shared/elements/button/button.component.ts
+++ library/src/main/ts/libraryBrs/src/app/shared/elements/button/button.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
-  @Input() iconClass="";
+  @Input() iconClass:string="";
   @Input() text="";
   @Input() btnClass="";
 
