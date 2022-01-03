84bb57a67e2fa741320286cc5dcfec705948154b
Monika Kordoń <monikor97@gmail.com>
2022-01-04 00:39:17 +0100

    #10 book routing

--- library/src/main/ts/libraryBrs/src/app/book/book-details/book-details.component.ts
+++ library/src/main/ts/libraryBrs/src/app/book/book-details/book-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-book-details',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-details.component.scss']
 })
 export class BookDetailsComponent implements OnInit {
-
-  constructor() { }
+  id: any = 0;
+  ids: any;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getBookId();
+  }
+
+  getBookId() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    console.log(this.id)
   }
 
 }
--- library/src/main/ts/libraryBrs/src/app/book/book-routing.module.ts
+++ library/src/main/ts/libraryBrs/src/app/book/book-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookDetailsComponent } from './book-details/book-details.component';
 import { BookSearchComponent } from './book-search/book-search.component';
 import { MyBooksComponent } from './my-books/my-books.component';
 
@@ -10,6 +11,7 @@ const routes: Routes = [
 
   { path: 'search',  component: BookSearchComponent },
   { path: 'myLists',  component: MyBooksComponent },
+  { path: 'details/:id',  component: BookDetailsComponent },
   { path: '**',  component: BookSearchComponent }
 ];
 
