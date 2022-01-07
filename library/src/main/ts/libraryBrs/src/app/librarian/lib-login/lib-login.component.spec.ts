4082582b0ad26bce040d83fbc1ca0592eafb47fe
Monika Kordoń <monikor97@gmail.com>
2022-01-07 17:42:16 +0100

    #12 librarian login component

---
+++ library/src/main/ts/libraryBrs/src/app/librarian/lib-login/lib-login.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LibLoginComponent } from './lib-login.component';
+
+describe('LibLoginComponent', () => {
+  let component: LibLoginComponent;
+  let fixture: ComponentFixture<LibLoginComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LibLoginComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LibLoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ library/src/main/ts/libraryBrs/src/app/librarian/lib-login/lib-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lib-login',
+  templateUrl: './lib-login.component.html',
+  styleUrls: ['./lib-login.component.scss']
+})
+export class LibLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
