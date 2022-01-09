f54e4868556f42cb55b8429a68f6b8874bb049ee
Monika Kordoń <monikor97@gmail.com>
2022-01-09 09:31:12 +0100

    #13 controller example methods

--- library/src/main/java/com/library/api/resource/BorrowingsResource.java
+++ library/src/main/java/com/library/api/resource/BorrowingsResource.java
@@ -1,10 +1,19 @@
 package com.library.api.resource;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.library.dto.BookDto;
+import com.library.response.Response;
 
 @RestController
 @RequestMapping("/api/borrowings")
 public interface BorrowingsResource {
-
+	/*@GetMapping("/newest")
+	ResponseEntity<Response<List<BookDto>>> getNewest(@RequestParam Long number);*/
 }
