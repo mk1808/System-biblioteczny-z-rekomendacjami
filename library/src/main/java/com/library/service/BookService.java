e0054cecb45b70d6a228a61eef3507367ed21cef
Monika Kordoń <monikor97@gmail.com>
2022-01-10 23:41:30 +0100

    #13 adding book service classes

--- library/src/main/java/com/library/service/BookService.java
+++ library/src/main/java/com/library/service/BookService.java
@@ -1,7 +1,41 @@
 package com.library.service;
 
+import java.util.List;
+
 import com.library.model.Book;
+import com.library.model.BookCopy;
+import com.library.model.ChangeProposal;
+import com.library.model.Opinion;
+import com.library.model.UserListElement;
 
 public interface BookService extends RepositoryService<Book>{
+
+	Book getByBookCopy(Long bookCopyId);
+
+	void createOpinion(Opinion opinion);
+
+	List<Opinion> getOpinionsByBookId(Long id);
+
+	Opinion getOpinionByBookIdAndUserId(Long userId, Long bookId);
+
+	List<Opinion> getOpinionsByUser(Long userId);
+
+	void updateOpinion(Opinion opinion);
+
+	List<BookCopy> getBookCopiesByBookId(Long id);
+
+	void createChangeProposals(List<ChangeProposal> changeProposals);
+
+	List<ChangeProposal> getChangeProposalsByBookId(Long bookId);
+
+	List<ChangeProposal> getNewChangeProposals();
+
+	void updateChangePorposal(ChangeProposal changeProposal);
+
+	void createUserListElement(UserListElement userListElement);
+
+	List<UserListElement> getUserListElementByUserAndType(Long userId, String type);
+
+	void deleteUserListElement(Long elementId);
 
 }
--- library/src/main/java/com/library/service/impl/BookServiceImpl.java
+++ library/src/main/java/com/library/service/impl/BookServiceImpl.java
@@ -1,11 +1,19 @@
 package com.library.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.library.model.Book;
+import com.library.model.BookCopy;
+import com.library.model.ChangeProposal;
+import com.library.model.Opinion;
+import com.library.model.UserListElement;
 import com.library.repository.BookRepository;
 import com.library.service.BookService;
 
+@Service
 public class BookServiceImpl implements BookService {
 private BookRepository repository;
 	
@@ -29,6 +37,104 @@ private BookRepository repository;
 	@Override
 	public Book update(Book entity) {
 		return repository.save(entity);
+	}
+
+
+	@Override
+	public Book getByBookCopy(Long bookCopyId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void createOpinion(Opinion opinion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public List<Opinion> getOpinionsByBookId(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Opinion getOpinionByBookIdAndUserId(Long userId, Long bookId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public List<Opinion> getOpinionsByUser(Long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void updateOpinion(Opinion opinion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public List<BookCopy> getBookCopiesByBookId(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void createChangeProposals(List<ChangeProposal> changeProposals) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public List<ChangeProposal> getChangeProposalsByBookId(Long bookId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public List<ChangeProposal> getNewChangeProposals() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void updateChangePorposal(ChangeProposal changeProposal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void createUserListElement(UserListElement userListElement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public List<UserListElement> getUserListElementByUserAndType(Long userId, String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void deleteUserListElement(Long elementId) {
+		// TODO Auto-generated method stub
+		
 	}
 
 
