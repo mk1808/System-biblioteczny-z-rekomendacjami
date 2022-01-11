f3f4493046b4129339676b6670778015567209c2
Monika Kordoń <monikor97@gmail.com>
2022-01-11 21:41:46 +0100

    #14 adding dto

--- library/src/main/java/com/library/dto/BookDto.java
+++ library/src/main/java/com/library/dto/BookDto.java
@@ -1,29 +1,37 @@
 package com.library.dto;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
+
+import com.library.enums.Role;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class BookDto {
 
-	private Long id;
+	private UUID id;
 	private String ISBN;
 	private String title;
 	private String publicationYear;
 	private String originalTitle;
 	private String descrpition;
 	private String photo;
-	private Long publisherId;
+	private UUID publisherId;
 	private String publisherName;
 	private List<AuthorDto> authors;
 	private List<GenreDto> genres;
 	private List<SeriesDto> series;
-	private List<String> keyWords;
+	private List<KeyWordDto> keyWords;
 	
 	
 }
