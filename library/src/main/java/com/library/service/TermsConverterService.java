141a4ae4a11384384b6209def48f73d3a80b35c0
Monika Kordoń <monikor97@gmail.com>
2022-01-11 01:04:15 +0100

    #13 terms converter

---
+++ library/src/main/java/com/library/service/TermsConverterService.java
@@ -0,0 +1,8 @@
+package com.library.service;
+
+import com.library.dto.TermsDto;
+import com.library.model.Terms;
+
+public interface TermsConverterService extends ConverterService <TermsDto, Terms>{
+
+}
---
+++ library/src/main/java/com/library/service/impl/TermsConverterServiceImpl.java
@@ -0,0 +1,51 @@
+package com.library.service.impl;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import com.library.dto.TermsDto;
+import com.library.model.Terms;
+import com.library.service.TermsConverterService;
+
+@Service
+public class TermsConverterServiceImpl implements TermsConverterService {
+
+	@Override
+	public TermsDto toDto(Terms model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Terms toModel(TermsDto dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TermsDto> toDtoList(List<Terms> models) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Terms> toModelList(List<TermsDto> dtos) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<TermsDto> toDtoPage(Page<Terms> models) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<Terms> toModelPage(Page<TermsDto> dtos) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
