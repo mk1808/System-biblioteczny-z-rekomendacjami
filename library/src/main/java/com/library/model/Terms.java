2626414cdb7badc43577015469378e122b04b88c
Monika Kordoń <monikor97@gmail.com>
2022-01-11 21:43:19 +0100

    #14 fixing builders in models

--- library/src/main/java/com/library/model/Terms.java
+++ library/src/main/java/com/library/model/Terms.java
@@ -9,12 +9,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@SuperBuilder
 @Entity
 public class Terms extends BaseEntity {
 	
