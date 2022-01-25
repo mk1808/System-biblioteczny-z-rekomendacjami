214e25b944955943778c32322e4752a945af719e
Monika Kordoń <monikor97@gmail.com>
2022-01-25 02:52:23 +0100

    #12 adding book component and routing

--- library/src/main/ts/libraryBrs/src/app/librarian/lib-book/lib-book-routing.module.ts
+++ library/src/main/ts/libraryBrs/src/app/librarian/lib-book/lib-book-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddBookComponent } from './add-book/add-book.component';
+import { LibBookDetailsComponent } from './lib-book-details/lib-book-details.component';
+import { LibBookSearchComponent } from './lib-book-search/lib-book-search.component';
 
 
 const routes: Routes = [
 
- // { path: 'login', component: LibLoginComponent }
+  { path: 'new', component: AddBookComponent },
+  { path: 'search', component: LibBookSearchComponent },
+  { path: 'details', component: LibBookDetailsComponent },
+  
+  { path: '**', component: AddBookComponent },
 ];
 
 @NgModule({
--- library/src/main/ts/libraryBrs/src/app/librarian/lib-book/lib-book.module.ts
+++ library/src/main/ts/libraryBrs/src/app/librarian/lib-book/lib-book.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddBookComponent } from './add-book/add-book.component';
+import { LibBookRoutingModule } from './lib-book-routing.module';
+import { LibBookDetailsComponent } from './lib-book-details/lib-book-details.component';
+import { LibBookSearchComponent } from './lib-book-search/lib-book-search.component';
+import { AddBookCopyComponent } from './add-book/add-book-copy/add-book-copy.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddBookComponent,
+    LibBookDetailsComponent,
+    LibBookSearchComponent,
+    AddBookCopyComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    LibBookRoutingModule
   ]
 })
 export class LibBookModule { }
