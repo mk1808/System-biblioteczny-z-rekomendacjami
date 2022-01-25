ce1ee394b11f0cbbc75bdb5211dd74f23304a2e4
Monika Kordoń <monikor97@gmail.com>
2022-01-25 23:19:09 +0100

    #5 multi select fix

--- library/src/main/ts/libraryBrs/src/app/shared/elements/multi-select/multi-select.component.ts
+++ library/src/main/ts/libraryBrs/src/app/shared/elements/multi-select/multi-select.component.ts
@@ -1,15 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+declare let $: any;
+declare let jQuery: any;
 @Component({
   selector: 'app-multi-select',
   templateUrl: './multi-select.component.html',
   styleUrls: ['./multi-select.component.scss']
 })
-export class MultiSelectComponent implements OnInit {
-  @Input() label:any;
+export class MultiSelectComponent implements OnInit, AfterViewInit {
+  @Input() label: any;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.loadJQuery();
+  }
+
+  loadJQuery() {
+    var element = $("#" + this.label).find('.search.dropdown');
+    element.dropdown();
   }
 
 }
