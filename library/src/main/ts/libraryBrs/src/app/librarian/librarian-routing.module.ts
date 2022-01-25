8af74103c0d5687cfd8fff1688f2c089f9321413
Monika Kordoń <monikor97@gmail.com>
2022-01-25 02:49:31 +0100

    #12 module fix

--- library/src/main/ts/libraryBrs/src/app/librarian/librarian-routing.module.ts
+++ library/src/main/ts/libraryBrs/src/app/librarian/librarian-routing.module.ts
@@ -5,6 +5,7 @@ import { LibLoginComponent } from './lib-auth/lib-login/lib-login.component';
 const routes: Routes = [
 
   { path: 'auth', loadChildren: () => import('./lib-auth/lib-auth.module').then(m => m.LibAuthModule) },
+  { path: 'books', loadChildren: () => import('./lib-book/lib-book.module').then(m => m.LibBookModule) },
 ];
 
 @NgModule({
--- library/src/main/ts/libraryBrs/src/app/librarian/librarian.module.ts
+++ library/src/main/ts/libraryBrs/src/app/librarian/librarian.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LibrarianRoutingModule } from './librarian-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -9,7 +10,8 @@ import { LibrarianRoutingModule } from './librarian-routing.module';
   ],
   imports: [
     CommonModule,
-    LibrarianRoutingModule
+    LibrarianRoutingModule,
+    SharedModule
   ]
 })
 export class LibrarianModule { }
