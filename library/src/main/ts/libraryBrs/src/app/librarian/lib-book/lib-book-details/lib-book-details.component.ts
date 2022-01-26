50be2cf797080992c500e0a9af3924f2c44ac4ce
Monika Kordoń <monikor97@gmail.com>
2022-01-26 22:26:25 +0100

    #12 book details - copies tab

--- library/src/main/ts/libraryBrs/src/app/librarian/lib-book/lib-book-details/lib-book-details.component.ts
+++ library/src/main/ts/libraryBrs/src/app/librarian/lib-book/lib-book-details/lib-book-details.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { I18NEXT_SERVICE, ITranslationService } from 'angular-i18next';
 
 @Component({
   selector: 'app-lib-book-details',
@@ -35,9 +36,17 @@ export class LibBookDetailsComponent implements OnInit {
   comment = {username:"Użytkownik 1", content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed enim in nunc feugiat euismod vitae necnunc. Morbi ac massa ut sem hendrerit", 
   rating:6, date:"05.01.2022r."};
   comments = [this.comment, this.comment, this.comment, this.comment]
+ 
+
+  accHeaders=this.getAccorditionHeaders();
+  headers=this.getHeaders();
+  text1=""
+  deleteText=this.getTranslation("booklist.table.delete")
+  prolongText=this.getTranslation("booklist.table.prolong")
+  resignText=this.getTranslation("booklist.table.resign")
 
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, @Inject(I18NEXT_SERVICE) private i18NextService: ITranslationService) { }
 
   ngOnInit(): void {
     this.getBookId();
@@ -46,6 +55,31 @@ export class LibBookDetailsComponent implements OnInit {
   getBookId() {
     this.id = this.route.snapshot.paramMap.get('id');
     console.log(this.id)
+  }
+
+  getTranslation(key:String){
+    return this.i18NextService.getResource("pl", "translation", key, "");
+  }
+
+  getAccorditionHeaders() {
+    return {
+      borrowed: this.getTranslation("booklist.borrowed"), 
+      borrowedInPast: this.getTranslation("booklist.borrowedInPast"),
+      reserved: this.getTranslation("booklist.reserved"), 
+      toReadList: this.getTranslation("booklist.toReadList"), 
+      favList: this.getTranslation("booklist.favList"), 
+      rated: this.getTranslation("booklist.rated")
+    }
+  }
+  getHeaders() {
+    return {
+      borrowed: this.getTranslation("booklist.borrowed"), 
+      borrowedInPast: this.getTranslation("booklist.borrowedInPast"),
+      reserved: this.getTranslation("booklist.reserved"), 
+      toReadList: this.getTranslation("booklist.toReadList"), 
+      favList: this.getTranslation("booklist.favList"), 
+      rated: this.getTranslation("booklist.rated")
+    }
   }
 
 }
