9081aeb051c7f8220e6722d94cdcf069075570fe
Monika Kordoń <monikor97@gmail.com>
2022-01-26 05:19:38 +0100

    #12 code format

--- library/src/main/ts/libraryBrs/src/app/book/book-search/book-search.component.ts
+++ library/src/main/ts/libraryBrs/src/app/book/book-search/book-search.component.ts
@@ -6,24 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-search.component.scss']
 })
 export class BookSearchComponent implements OnInit {
-  title:any = "booksSearch";
-  iconClass="search"
-  text="search"
+  title: any = "booksSearch";
+  iconClass = "search"
+  text = "search"
   label = "Tytuł"
-  btnClass="full"
-  no=0;
-  book={id:0, photo:'https://picsum.photos/200/300', title:'Lorem ipsum dolor', author:'Dolores Gibson'}
-  books=[this.getBook(),this.getBook(),this.getBook(),this.getBook(),this.getBook(),this.getBook(),this.getBook() ];
-  
+  btnClass = "full"
+  no = 0;
+  book = { id: 0, photo: 'https://picsum.photos/200/300', title: 'Lorem ipsum dolor', author: 'Dolores Gibson' }
+  books = [this.getBook(), this.getBook(), this.getBook(), this.getBook(), this.getBook(), this.getBook(), this.getBook()];
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  getBook(){
+  getBook() {
     this.no++;
-    this.book.id=this.no;
+    this.book.id = this.no;
     return this.book;
   }
 }
