f3d6083b712b3ebfc26f521bee1278f97afd452e
Monika Kordoń <monikor97@gmail.com>
2022-01-26 05:20:40 +0100

    #12 search component

--- library/src/main/ts/libraryBrs/src/app/librarian/lib-book/lib-book-search/lib-book-search.component.ts
+++ library/src/main/ts/libraryBrs/src/app/librarian/lib-book/lib-book-search/lib-book-search.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibBookSearchComponent implements OnInit {
 
+  title: any = "booksSearch";
+  iconClass = "search"
+  text = "search"
+  label = "Tytuł"
+  btnClass = "full"
+  no = 0;
+  book = { id: 0, photo: 'https://picsum.photos/200/300', title: 'Lorem ipsum dolor', author: 'Dolores Gibson' }
+  books = [this.getBook(), this.getBook(), this.getBook(), this.getBook(), this.getBook(), this.getBook(), this.getBook()];
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getBook() {
+    this.no++;
+    this.book.id = this.no;
+    return this.book;
   }
 
 }
--- library/src/main/ts/libraryBrs/src/app/librarian/lib-book/lib-book.module.ts
+++ library/src/main/ts/libraryBrs/src/app/librarian/lib-book/lib-book.module.ts
@@ -6,6 +6,7 @@ import { LibBookDetailsComponent } from './lib-book-details/lib-book-details.com
 import { LibBookSearchComponent } from './lib-book-search/lib-book-search.component';
 import { AddBookCopyComponent } from './add-book/add-book-copy/add-book-copy.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { LibBookSearchElementComponent } from './lib-book-search/lib-book-search-element/lib-book-search-element.component';
 
 
 
@@ -14,7 +15,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
     AddBookComponent,
     LibBookDetailsComponent,
     LibBookSearchComponent,
-    AddBookCopyComponent
+    AddBookCopyComponent,
+    LibBookSearchElementComponent
   ],
   imports: [
     CommonModule,
