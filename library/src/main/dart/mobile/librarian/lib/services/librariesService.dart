edea58b48432fcb9299772d5a0a99c2e3107c726
Monika Kordoń <monikor97@gmail.com>
2022-01-30 02:33:52 +0100

    #18 methods in rest services

--- library/src/main/dart/mobile/librarian/lib/services/librariesService.dart
+++ library/src/main/dart/mobile/librarian/lib/services/librariesService.dart
@@ -2,9 +2,35 @@ import 'dart:collection';
 
 import 'package:flutter/cupertino.dart';
 import 'package:librarian/services/api/api.dart';
+import 'package:librarian/services/restService.dart';
 
 class LibrariesService extends ChangeNotifier {
+  final String _url = "api/libraries";
   final List<Library> _libraries = [];
 
   UnmodifiableListView<Library> get items => UnmodifiableListView(_libraries);
+
+  void getContact() {
+    RestService rest = RestService();
+    rest.get<dynamic>(
+        path: "${_url}/contact", onSuccess: onSuccessGetContact);
+  }
+
+  void onSuccessGetContact(dynamic object) {
+    print(object);
+    Response response = Response.fromJson(object);
+    notifyListeners();
+  }
+
+  void getTerms() {
+    RestService rest = RestService();
+    rest.get<dynamic>(
+        path: "${_url}/terms", onSuccess: onSuccessGetTerms);
+  }
+
+  void onSuccessGetTerms(dynamic object) {
+    print(object);
+    Response response = Response.fromJson(object);
+    notifyListeners();
+  }
 }
