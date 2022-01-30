e5769d3a1262493151a29dd48f4c02c175510102
Monika Kordoń <monikor97@gmail.com>
2022-01-30 11:03:32 +0100

    #5 making input element as reactive form

--- library/src/main/ts/libraryBrs/src/app/shared/elements/input/input.component.ts
+++ library/src/main/ts/libraryBrs/src/app/shared/elements/input/input.component.ts
@@ -1,25 +1,52 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { I18nService } from 'src/app/core/services/i18n.service';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: InputComponent
+    }
+  ]
 })
-export class InputComponent implements OnInit {
-  @Input() label:any;
-  @Input() inputClass:any;
-  @Input() type:any;
-  @Input() disabled:any;
-  disabledClass='';
-  labelTranslated =""
-  
+export class InputComponent implements OnInit, ControlValueAccessor {
+  @Input() label: any;
+  @Input() inputClass: any;
+  @Input() type: any;
+  @Input() disabled: any;
+  @Input() formControlName: any;
+  disabledClass = '';
+  labelTranslated = ""
+
+  value = ""
+  onChange = (quantity: any) => { };
+  onTouched = () => { };
+
   constructor(private i18nService: I18nService) { }
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+  registerOnChange(onChange: any): void {
+    this.onChange = onChange;
+  }
+  registerOnTouched(onTouched: any): void {
+    this.onTouched = onTouched;
+  }
 
   ngOnInit(): void {
     this.labelTranslated = this.i18nService.getTranslation(this.label);
-    this.type=this.type?this.type:'text';
-    this.disabledClass = this.disabled?'disabled':''
+    this.type = this.type ? this.type : 'text';
+    this.disabledClass = this.disabled ? 'disabled' : ''
+  }
+
+  change() {
+    this.onChange(this.value)
   }
 
 }
