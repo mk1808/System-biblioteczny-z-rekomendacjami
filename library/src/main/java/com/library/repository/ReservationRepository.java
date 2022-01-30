5f5764c110feaf230274951e0119d01a3b1d16b4
Monika Kordoń <monikor97@gmail.com>
2022-01-30 15:45:07 +0100

    #20 correcting reservations

--- library/src/main/java/com/library/repository/ReservationRepository.java
+++ library/src/main/java/com/library/repository/ReservationRepository.java
@@ -1,11 +1,14 @@
 package com.library.repository;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.library.model.Reservation;
 
 @Repository
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-
+public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+	List<Reservation> getByUserId(UUID userId);
 }
--- library/src/main/java/com/library/service/impl/ReservationServiceImpl.java
+++ library/src/main/java/com/library/service/impl/ReservationServiceImpl.java
@@ -2,37 +2,52 @@ package com.library.service.impl;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.library.model.Borrowing;
 import com.library.model.Reservation;
+import com.library.repository.ReservationRepository;
+import com.library.service.BookService;
 import com.library.service.ReservationService;
 
 @Service
 public class ReservationServiceImpl implements ReservationService {
-
+	private final ReservationRepository repository;
+	
+	@Autowired
+	public ReservationServiceImpl(ReservationRepository repository, BookService bookService) {
+		this.repository = repository;
+	}
+	
 	@Override
 	public Reservation get(UUID id) {
-		// TODO Auto-generated method stub
-		return null;
+		return repository.getById(id);
 	}
 
 	@Override
 	public Reservation create(Reservation entity) {
-		// TODO Auto-generated method stub
-		return null;
+		return repository.save(entity);
 	}
 
 	@Override
 	public Reservation update(Reservation entity) {
-		// TODO Auto-generated method stub
-		return null;
+		return repository.save(entity);
 	}
 
 	@Override
 	public List<Reservation> getByUserId(UUID userId) {
-		// TODO Auto-generated method stub
-		return null;
+
+		return repository.getByUserId(userId)
+				.stream()
+				.filter(this::wasNotBorrowed)
+				.collect(Collectors.toList());
+	}
+	
+	private Boolean wasNotBorrowed(Reservation reservation) {
+		return reservation.getWasBorrowed()==null||reservation.getWasBorrowed()==false;
 	}
 
 	@Override
