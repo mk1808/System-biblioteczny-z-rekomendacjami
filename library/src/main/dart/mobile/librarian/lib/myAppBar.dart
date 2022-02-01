8a256974a391ae580e99d2e934d4f2cf38953eee
Monika Kordoń <monikor97@gmail.com>
2022-02-01 18:47:30 +0100

    #18 mobile app - appBar

---
+++ library/src/main/dart/mobile/librarian/lib/myAppBar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:librarian/consts.dart';
+
+class MyAppBar extends StatelessWidget with PreferredSizeWidget {
+  const MyAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        title: Row(
+          children: [
+            //  MyLogo(),
+            Text(readerApp)
+          ],
+        ),
+        backgroundColor: mainColor,
+        foregroundColor: Colors.white);
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
