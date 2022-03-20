edc30ad52eaa26a399f3f3d908a87e43a57b9153
Monika Kordoń <monikor97@gmail.com>
2022-03-20 03:34:13 +0100

    #3 nav on main page

--- library/src/main/ts/libraryBrs/src/app/main-pages/main/main.component.ts
+++ library/src/main/ts/libraryBrs/src/app/main-pages/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  list(){
+    this.router.navigate(['/books/search']);
+  }
+
+  contact(){
+    this.router.navigate(['/home/contact']);
+  }
+
+  borrowingRules(){
+    this.router.navigate(['/user/myAccount']);
+  }
+
+  login(){
+    this.router.navigate(['/auth/login']);
   }
 
 }
