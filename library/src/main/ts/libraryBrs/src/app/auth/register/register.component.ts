695de9627d69f4cc6f247bc9b102190998f92d26
Monika Kordoń <monikor97@gmail.com>
2022-03-31 23:54:50 +0200

    #6 registration frontend request

--- library/src/main/ts/libraryBrs/src/app/auth/register/register.component.ts
+++ library/src/main/ts/libraryBrs/src/app/auth/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Address, Register } from 'src/app/core/services/rest/api/api';
+import { UsersService } from 'src/app/core/services/rest/users.service';
 
 @Component({
   selector: 'app-register',
@@ -30,11 +34,60 @@ export class RegisterComponent implements OnInit {
   flatNoInput="flatNo"
   postalCodeInput="postalCode"
   cityInput="city"
+  name="name";
+  surname="surname";
+  mail="mail";
+  phoneNo="phoneNo";
+  passwordRepeat="passwordRepeat";
+  password="password";
+  street="street";
+  houseNo="houseNo";
+  flatNo="flatNo";
+  postcode="postcode";
+  city="city";
+  submitBtnType="submit";
+  registerForm:FormGroup = new FormGroup({});
+
 
   
-  constructor() { }
+  constructor(private usersService:UsersService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.registerForm = new FormGroup({
+      mail: new FormControl(''),
+      password: new FormControl(''),
+      phoneNo: new FormControl(''),
+      name: new FormControl(''),
+      surname: new FormControl(''),
+      passwordRepeat: new FormControl(''),
+      street: new FormControl(''),
+      houseNo: new FormControl(''),
+      flatNo: new FormControl(''),
+      postcode: new FormControl(''),
+      city: new FormControl(''),
+    });
+  }
+
+  onSubmit() {
+    let register: Register = this.registerForm.value;
+    let address:Address = this.registerForm.value;
+    register.address = {'street': this.registerForm.value.street,
+      'houseNo': this.registerForm.value.houseNo,
+      'flatNo': this.registerForm.value.flatNo,
+      'postcode': this.registerForm.value.postcode,
+      'city': this.registerForm.value.city,
+      'district': this.registerForm.value.district,
+      'country': this.registerForm.value.country}
+
+  this.usersService.register(register).subscribe(resp => {
+
+      //this.router.navigate(['/user/myAccount']);
+      console.log(resp)
+    })
   }
 
 }
