1d3d90977bc9f78d0d8974a5e317b1448ad02312
Monika Kordoń <monikor97@gmail.com>
2022-04-09 14:47:41 +0200

    #10 add to list modal

--- library/src/main/ts/libraryBrs/src/app/book/book-details/add-to-list-modal/add-to-list-modal.component.ts
+++ library/src/main/ts/libraryBrs/src/app/book/book-details/add-to-list-modal/add-to-list-modal.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormatterService } from 'src/app/core/services/formatter.service';
+import { Book, Reservation, UserListElement } from 'src/app/core/services/rest/api/api';
+import { ReservationsService } from 'src/app/core/services/rest/reservations.service';
+import { BooksService } from 'src/app/core/services/rest/books.service';
 
 @Component({
   selector: 'app-add-to-list-modal',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddToListModalComponent implements OnInit {
 
-  constructor() { }
+  @Input() book: Book={};
+  @Input() typeOfList:string="";
+  title = "addToList.confirm";
+  question="";
+  questionFav = "addToListFavourite.confirm.question";
+  questionToRead = "addToListToRead.confirm.question";
+  userListElement:UserListElement={};
+
+  constructor(private formatterService:FormatterService, public activeModal: NgbActiveModal, public booksService:BooksService) { }
 
   ngOnInit(): void {
+    this.question = this.typeOfList==="f"?this.questionFav:this.questionToRead;
+    console.log(this.book)
   }
 
+  onCancel=()=>{
+    debugger;
+    this.activeModal.dismiss('Cross click');
+
+  }
+
+  onConfirm=()=>{
+    this.userListElement.bookId = this.book.id;
+    this.userListElement.type = this.typeOfList;
+    this.userListElement.userId = "a5edd677-14a9-449b-ab3f-76742762c5f1";
+    console.log(this.userListElement)
+    this.booksService.createUserListElement(this.userListElement).subscribe(resp=>{
+      console.log(resp);
+        this.onCancel();
+    })
+  
+  }
+
+  formatAuthors(list: Object[] | undefined) {
+    if (list) { return this.formatterService.displayList(list); }
+    return "-"
+  }
 }
