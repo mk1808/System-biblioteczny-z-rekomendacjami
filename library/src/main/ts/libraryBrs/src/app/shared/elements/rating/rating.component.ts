397facd45f2450ed9abf167a592afd6455e398a3
Monika Kordoń <monikor97@gmail.com>
2022-04-15 21:26:51 +0200

    #5 rating component - enable working as form control

--- library/src/main/ts/libraryBrs/src/app/shared/elements/rating/rating.component.ts
+++ library/src/main/ts/libraryBrs/src/app/shared/elements/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rating',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rating.component.scss']
 })
 export class RatingComponent implements OnInit {
+
+  @Input() ratingFormCtrl:any;
   currentRate = 6;
   constructor() { }
 
