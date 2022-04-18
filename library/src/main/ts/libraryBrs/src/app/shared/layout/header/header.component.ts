0bdfb7aaa3ecbd366dd4eceba5fd64e9d61ab0b8
Monika Kordoń <monikor97@gmail.com>
2022-04-18 12:17:58 +0200

    code format

--- library/src/main/ts/libraryBrs/src/app/shared/layout/header/header.component.ts
+++ library/src/main/ts/libraryBrs/src/app/shared/layout/header/header.component.ts
@@ -1,35 +1,37 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { UsersService } from 'src/app/core/services/rest/users.service';
-declare let $ : any;
+declare let $: any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit, AfterViewInit {
-  logged:BehaviorSubject<Boolean>;
-  constructor(private usersService:UsersService) { 
-    this.logged=this.usersService.logged;
-    this.logged.subscribe(x=>{//debugger;
-      console.log(x)})
+  logged: BehaviorSubject<Boolean>;
+  
+  constructor(private usersService: UsersService) {
+    this.logged = this.usersService.logged;
+    this.logged.subscribe(x => {//debugger;
+      console.log(x)
+    })
   }
 
   ngOnInit(): void {
   }
   ngAfterViewInit() {
-   
-    //let elements = this.element.nativeElement.querySelectorAll('.ui.dropdown');
-//    setTimeout(this.loadJQuery);
-    this.loadJQuery();
- }
 
- 
- loadJQuery(){
-   /*debugger
-   let select:any = $('.search.dropdown')[0];
-    select.dropdown();
-    debugger*/
+    //let elements = this.element.nativeElement.querySelectorAll('.ui.dropdown');
+    //    setTimeout(this.loadJQuery);
+    this.loadJQuery();
+  }
+
+
+  loadJQuery() {
+    /*debugger
+    let select:any = $('.search.dropdown')[0];
+     select.dropdown();
+     debugger*/
     $('.menu.dmenu').dropdown();
- }
+  }
 }
