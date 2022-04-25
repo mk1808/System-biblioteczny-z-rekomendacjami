24fa6319269a6e185174ad0c6c9463b411ecd8a9
Monika Kordoń <monikor97@gmail.com>
2022-04-25 20:49:32 +0200

    #24 aspects for creating book profile

--- library/src/main/java/com/library/aspect/BooksControllerAspect.java
+++ library/src/main/java/com/library/aspect/BooksControllerAspect.java
@@ -1,38 +1,42 @@
 package com.library.aspect;
 
-import static com.library.enums.Consts.BOOK_CTRL;
-import static com.library.enums.Consts.LIST_ELEM_DTO;
-import static com.library.enums.Consts.OPINION_DTO;
-import static com.library.enums.Consts.RESPONSE;
+import static com.library.enums.Consts.*;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 
+import com.library.dto.ChangeProposalDto;
 import com.library.dto.OpinionDto;
 import com.library.dto.UserListElementDto;
+import com.library.nosql.model.BookData;
 import com.library.nosql.model.UserData;
 import com.library.nosql.repository.BookDataRepository;
 import com.library.nosql.repository.UserDataRepository;
+import com.library.response.Response;
 
 @Aspect
 @Component
 public class BooksControllerAspect {
 	
-	private final BookDataRepository bookItemRepository;
+	private final BookDataRepository bookDataRepository;
 	private final UserDataRepository userDataRepository;
 	private final AspectHelperService helperService;
 	
 	@Autowired
 	public BooksControllerAspect(BookDataRepository bookItemRepository, UserDataRepository userDataRepository, AspectHelperService helperService) {
-		this.bookItemRepository = bookItemRepository;
+		this.bookDataRepository = bookItemRepository;
 		this.userDataRepository = userDataRepository;
 		this.helperService = helperService;
 	}
@@ -80,4 +84,30 @@ public class BooksControllerAspect {
 		userDataRepository.save(toSave);
 	} 
 	
+	//ResponseEntity<Response<String>> createChangeProposal(List<ChangeProposalDto> changeProposalsDto)
+	
+	@After(value = "target(" + BOOK_CTRL + ") && execution(" + RESPONSE + "String>> createChangeProposal("
+			+ LIST_CHANGE_PROPOSAL_DTO + ")) && args(changeProposalsDto)")
+	public void afterOpinionAdvice(JoinPoint joinPoint, List<ChangeProposalDto> changeProposalsDto) {
+
+		String bookId = changeProposalsDto.get(0).getBookId().toString();
+		List<String> keyWordsIds = new ArrayList();
+		for (ChangeProposalDto proposal : changeProposalsDto) {
+			if ("keyword".equals(proposal.getType())) {
+				keyWordsIds.add(proposal.getValue());
+			}
+		}
+
+		Optional<BookData> bookData = bookDataRepository.findById(bookId);
+		if (bookData.isPresent()) {
+			BookData book = bookData.get();
+			keyWordsIds.addAll(book.getKeyWordIds());
+			book.setKeyWordIds(keyWordsIds);
+			bookDataRepository.save(book);
+		} else {
+			BookData toSave = BookData.builder().id(bookId).keyWordIds(keyWordsIds).build();
+			bookDataRepository.save(toSave);
+		}
+	}
+
 }
