d3769966fcacc3b73e9da9389a59bfd6c0e02a1c
Monika Kordoń <monikor97@gmail.com>
2022-04-25 19:11:21 +0200

    #24 following user - getting user info from request

--- library/src/main/java/com/library/aspect/BooksControllerAspect.java
+++ library/src/main/java/com/library/aspect/BooksControllerAspect.java
@@ -1,45 +1,87 @@
 package com.library.aspect;
 
+import java.util.Base64;
+import java.util.Date;
 import java.util.UUID;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.library.dto.BookDto;
+import com.library.nosql.model.UserData;
+import com.library.nosql.repository.BookDataRepository;
+import com.library.nosql.repository.UserDataRepository;
 import com.library.response.Response;
+import static com.library.enums.Consts.*;
 
 @Aspect
 @Component
 public class BooksControllerAspect {
-//public ResponseEntity<Response<BookDto>> getById(UUID id)
 	
-//	@Pointcut("execution(* com.library.api.controller.BooksController.*.*(..))")
-  //  public void serviceMethodExecution(){
-    //}
+	private final BookDataRepository bookItemRepository;
+	private final UserDataRepository userDataRepository;
+	
+	@Autowired
+	public BooksControllerAspect(BookDataRepository bookItemRepository, UserDataRepository userDataRepository) {
+		this.bookItemRepository = bookItemRepository;
+		this.userDataRepository = userDataRepository;
+	}
+	
 
-	//@After(value = "getById() && args(id)")
 	
-	@After(value = "target(com.library.api.controller.BooksController) && execution(org.springframework.http.ResponseEntity<com.library.response.Response<com.library.dto.BookDto>> getById(java.util.UUID, javax.servlet.http.HttpServletRequest))")  
-	public void afterAdvice(JoinPoint joinPoint) {  
+	@After(value = "target(com.library.api.controller.BooksController) && execution(org.springframework.http.ResponseEntity<com.library.response.Response<com.library.dto.BookDto>> getById(java.util.UUID)) && args(id)")  
+	public void afterAdvice(JoinPoint joinPoint, UUID id) {  
 		RequestContextHolder.getRequestAttributes().getAttribute("response", 3); 
-		
-		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-		request.getAttribute("testname");
-	System.out.println("After method:" + joinPoint.getSignature()); 
-	joinPoint.getArgs();
-	System.out.println(request.getCookies()[0].getName().toString());
-	System.out.println(request.getCookies()[0].getValue().toString());
-			
-	request.getCookies()[0].getName().toString();
-	//System.out.println("getting - "+id);  
+		String userEmail = getUserEmailFromRequest(joinPoint);
+		UserData toSave = UserData.builder()
+				.action("get")
+				.bookId(id.toString())
+				.userId(userEmail)
+				.date((new Date()).getTime())
+				.build();
+		userDataRepository.save(toSave);
 	} 
+	
+	private String encodeUser(String jwt){
+		Base64.Decoder decoder = Base64.getUrlDecoder();
+		String[] chunks = jwt.split("\\.");
+		String payload = new String(decoder.decode(chunks[1]));
+		return payload;
+	}
+	
+	private String getUserEmail(String user) {
+		String[] splitted = user.split("\"sub\":\"");
+		String[] splitted2 = splitted[1].split("\"");
+		System.out.println(splitted2[0]);  
+		return splitted2[0];
+		
+	}
+	
+	private String getUserEmailFromCookie(Cookie authCookie) {
+		String userEncoded = authCookie.getValue().toString();
+		String userDecoded = encodeUser(userEncoded);
+		System.out.println("getting - "+userDecoded);  
+		return getUserEmail(userDecoded);
+		
+	}
 
+	private String getUserEmailFromRequest(JoinPoint joinPoint) {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+		Cookie authCookie = request.getCookies()[0];
+		if("Authorization".equals(authCookie.getName().toString())) {
+			return getUserEmailFromCookie(authCookie);
+		}
+		return "";
+		
+	}
 }
