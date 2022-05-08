13a2930aac5664a1789400401f57610281bef517
Monika Kordoń <monikor97@gmail.com>
2022-05-08 04:28:07 +0200

    #27 adding filters for librarian

--- library/src/main/ts/libraryBrs/src/app/librarian/lib-book/lib-book-search/lib-book-search.component.ts
+++ library/src/main/ts/libraryBrs/src/app/librarian/lib-book/lib-book-search/lib-book-search.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+import { Author, Book, BookFIlter, Genre, Publisher } from 'src/app/core/services/rest/api/api';
+import { BooksService } from 'src/app/core/services/rest/books.service';
+import { DictionaryService } from 'src/app/core/services/rest/dictionary.service';
 
 @Component({
   selector: 'app-lib-book-search',
@@ -12,15 +17,57 @@ export class LibBookSearchComponent implements OnInit {
   text = "search"
   label = "Tytuł"
   btnClass = "full"
+  bookTitle = "title";
+  bookCopyNo = "bookCopyNo";
+  ISBN = "ISBN"
   no = 0;
   book = { id: 0, photo: 'https://picsum.photos/200/300', title: 'Lorem ipsum dolor', author: 'Dolores Gibson' }
   books = [this.getBook(), this.getBook(), this.getBook(), this.getBook(), this.getBook(), this.getBook(), this.getBook()];
   shouldDisplayAlert = false;
+  submitBtnType = "submit";
+  searchForm: FormGroup = new FormGroup({});
+  genres: BehaviorSubject<Genre[]> = new BehaviorSubject<Genre[]>([]);
+  authors: BehaviorSubject<Author[]> = new BehaviorSubject<Author[]>([]);
+  publishers: BehaviorSubject<Publisher[]> = new BehaviorSubject<Publisher[]>([]);
+  booksFiltered: BehaviorSubject<Book[]> = new BehaviorSubject<Book[]>([]);
 
-  constructor() { }
+  constructor(private booksService: BooksService,
+    private dictionaryService: DictionaryService) { }
 
   ngOnInit(): void {
+    this.initForm();
+    this.updateDictionariesValues()
+    this.updateBook();
+  }
 
+  updateDictionariesValues() {
+    this.dictionaryService.getAuthors();
+    this.dictionaryService.getGenres();
+    this.dictionaryService.getPublishers();
+    this.authors = this.dictionaryService.authors;
+    this.genres = this.dictionaryService.genres;
+    this.publishers = this.dictionaryService.publishers;
+    console.log(this.authors)
+
+    console.log(this.publishers)
+    console.log(this.genres)
+
+  }
+
+  updateBook(){
+    this.booksService.getFiltered(0,0,{})
+    this.booksFiltered=this.booksService.searchedBooks;
+  }
+
+  initForm() {
+    this.searchForm = new FormGroup({
+      title: new FormControl(''),
+      ISBN: new FormControl(''),
+      authorId: new FormControl(''),
+      genreId: new FormControl(''),
+      publisherId: new FormControl(''),
+      bookCopyNo: new FormControl('')
+    });
   }
 
   getBook() {
@@ -36,5 +83,29 @@ export class LibBookSearchComponent implements OnInit {
   search(){
     this.shouldDisplayAlert = true;
   }
+
+  onSubmit(form: any) {
+    console.log(this.searchForm.value)
+    let bookFilter: BookFIlter = this.searchForm.value;
+    debugger;
+    bookFilter.authorId = this.replaceDefaultValue(bookFilter.authorId);
+    bookFilter.genreId = this.replaceDefaultValue(bookFilter.genreId);
+    bookFilter.publisherId = this.replaceDefaultValue(bookFilter.publisherId);
+   
+    this.booksService.getFiltered(0, 0, bookFilter);
+    this.booksFiltered=this.booksService.searchedBooks;
+
+
+  }
+
+  getAuthorDisplayedValue(option:any){
+   
+    return `${option.name} ${option.surname}`
+  
+}
+
+replaceDefaultValue(value:string|undefined){
+  return value=="0"?"":value;
+}
 
 }
