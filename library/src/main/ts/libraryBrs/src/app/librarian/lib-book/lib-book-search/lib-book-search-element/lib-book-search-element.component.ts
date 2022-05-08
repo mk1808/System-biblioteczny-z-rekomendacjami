12c8859e2cb069a894448a2e2ffdf1639b56665b
Monika Kordoń <monikor97@gmail.com>
2022-05-08 05:03:11 +0200

    #27 displaying books on searching view

--- library/src/main/ts/libraryBrs/src/app/librarian/lib-book/lib-book-search/lib-book-search-element/lib-book-search-element.component.ts
+++ library/src/main/ts/libraryBrs/src/app/librarian/lib-book/lib-book-search/lib-book-search-element/lib-book-search-element.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormatterService } from 'src/app/core/services/formatter.service';
+import { Author, Book } from 'src/app/core/services/rest/api/api';
 
 @Component({
   selector: 'app-lib-book-search-element',
   templateUrl: './lib-book-search-element.component.html',
   styleUrls: ['./lib-book-search-element.component.scss']
 })
-export class LibBookSearchElementComponent implements OnInit {
-
-  constructor() { }
+export class LibBookSearchElementComponent implements OnInit, OnChanges {
+  bookToDisplay: Book = {};
+  @Input() book: Book = {};
+  constructor(private router: Router, private formatterService: FormatterService) { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.bookToDisplay = this.book;
+    console.log(this.book)
+  }
+
+  getAuthorDisplayedValue(options: Author[] | undefined) {
+    let displayed = ""
+    if (options){
+      options.forEach(option => { displayed += `${option.name} ${option.surname}, ` }) ;
+      displayed = displayed.substring(0, displayed.length - 2);
+    }
+      
+   
+    return displayed;
+
+  }
+
+  onClick(){
+    this.router.navigate(['/books/details/'+this.book.id]);
+  }
+
+  formatOther(list: Object[] | undefined) {
+    if (list) { return this.formatterService.displayListNames(list); }
+    return "-";
   }
 
 }
