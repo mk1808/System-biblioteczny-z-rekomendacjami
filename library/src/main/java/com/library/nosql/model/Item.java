ce9084ef0b8fe113f9c0e5965f44fba202af5115
Monika Kordoń <monikor97@gmail.com>
2022-05-08 10:20:54 +0200

    #24 other way of saving user actions - tests

--- library/src/main/java/com/library/nosql/model/Item.java
+++ library/src/main/java/com/library/nosql/model/Item.java
@@ -1,5 +1,8 @@
 package com.library.nosql.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Id;
 
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -13,6 +16,7 @@ public class Item {
 	private String name;
 	private int quantity;
 	private String category;
+	private List<Subitem> subitems;;
 
 	public Item(String id, String name, int quantity, String category) {
 		super();
@@ -20,5 +24,16 @@ public class Item {
 		this.name = name;
 		this.quantity = quantity;
 		this.category = category;
+	}
+	
+	public Item withSubitems(List<Subitem> subitems) {
+		this.subitems = subitems;
+		return this;
+	}
+	
+	public Item withSubitem(Subitem subitem) {
+		if (this.subitems == null) {this.subitems = new ArrayList();}
+		this.subitems.add(subitem);
+		return this;
 	}
 }
---
+++ library/src/main/java/com/library/nosql/model/Subitem.java
@@ -0,0 +1,10 @@
+package com.library.nosql.model;
+
+public class Subitem {
+	private String newName;
+	
+	public Subitem(String newName) {
+		this.newName = newName;
+	}
+
+}
